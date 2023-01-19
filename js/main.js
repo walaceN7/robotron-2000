e2eccde81d174a5fe89ac5e9602c00262e509e36
Walace <117746323+walaceN7@users.noreply.github.com>
2023-01-19 14:15:49 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -62,3 +62,7 @@ function atualizaEstatisticas(peca, operacao){
         }
     })
 }
+
+function trocaImagem(cor){
+    document.querySelector(".robo").src="img/Robotron 2000 - " + cor + ".png";
+ }
